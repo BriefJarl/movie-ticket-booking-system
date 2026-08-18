2422f52f2db284c66801590e9bc87c8db2fe9129
BHUMIKA <bhumikashaw.549@gmail.com>
2026-08-18 09:06:56 +0530

    Update User.java

--- moviebooking/src/main/java/com/bhumi/moviebooking/entity/User.java
+++ moviebooking/src/main/java/com/bhumi/moviebooking/entity/User.java
@@ -12,30 +12,26 @@ public class User {
     private String name;
     private String email;
 
-    // GETTERS
+    //getters
 
     public Long getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public String getEmail() {
         return email;
     }
 
-    // SETTERS
+    //setters
 
     public void setId(Long id) {
         this.id = id;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
